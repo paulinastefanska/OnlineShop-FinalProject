fc632d2b06f1c141a415b409aa700e85b8a1f8fc
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-21 13:17:48 +0200

    Import and use router

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
 
 const Root = () => (
-  <App />
+	<Router>
+  	<App />
+  </Router>
 );
 
 ReactDOM.render(<Root />, document.getElementById('root'));
