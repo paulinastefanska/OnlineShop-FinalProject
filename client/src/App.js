1b31eb2e08b3564f64bd7122dc63bca023cbdbc7
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-21 13:52:18 +0200

    Prepare all subpages

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import MainLayout from './components/layout/MainLayout/MainLayout';
+
+// imports routes
+import Home from './components/pages/Home/HomePage';
+import Faq from './components/pages/Faq/FaqPage';
+import StorePolicy from './components/pages/StorePolicy/StorePolicyPage';
+import Contact from './components/pages/Contact/ContactPage';
+import Cart from './components/pages/Cart/CartPage';
+import NotFound from './components/pages/NotFound/NotFoundPage';
 
 class App extends React.Component {
 
-  state = {
-    posts: [],
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:8000/api/products')
-      .then(res => res.json())
-      .then(res => {
-        this.setState({ posts: res });
-    });
-  }
-
   render() {
     return (
-      <div>
-        <ul>
-            {this.state.posts.map(post => <li key={post.id}>{post.title}</li>)}
-        </ul>
-      </div>
+      <MainLayout>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/faq" exact component={Faq} />
+          <Route path="/storepolicy" exact component={StorePolicy} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/cart" exact component={Cart} />
+          <Route component={NotFound} />
+        </Switch>
+      </MainLayout>
     );
   }
 
