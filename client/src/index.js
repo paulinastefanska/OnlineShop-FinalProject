81dc0560e39b52ebe3b7c971e431d6efeeced069
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-23 09:30:59 +0200

    Import styles

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import './styles/global.scss';
 
 import App from './App';
 
