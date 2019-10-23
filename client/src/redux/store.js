896870df691156d139a1414e7a3ebb51f3b27b4a
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-23 08:48:32 +0200

    Add devtools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 // import reducers
@@ -10,6 +10,12 @@ const rootReducer = combineReducers({
 });
 
 // create store
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+	rootReducer, 
+	compose(
+		applyMiddleware(thunk),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
+);
 
 export default store;
