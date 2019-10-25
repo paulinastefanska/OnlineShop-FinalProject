8e6dae47044f37d40b82e503218617c908b0bdf3
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-25 11:10:14 +0200

    Configure server - refactoring

--- server/server.js
+++ server/server.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const config = require('./config');
 
 const app = express();
+
+// import routes
+const productRoutes = require('./routes/product.routes');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use('/api', productRoutes);
 
-app.get('/api/products', (req, res) => {
-  const data = [
-    { id: '1adfasf', title: 'Product 1', content: 'Photo 1' },
-    { id: '2evxc34', title: 'Product 2', content: 'Photo 2' },
-  ]
-  res.json(data);
-});
-
-app.listen(8000, function(){
-  console.log('Server is running on port:', 8000);
+app.listen(config.PORT, function(){
+  console.log('Server is running on port:', config.PORT);
 });
