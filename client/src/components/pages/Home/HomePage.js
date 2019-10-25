7f2a863c68721a9ae6d0dfa0a6bef70974f6b36e
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-25 10:09:11 +0200

    Add styles and use bootstrap

--- client/src/components/pages/Home/HomePage.js
+++ client/src/components/pages/Home/HomePage.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 import ProductsCounter from '../../features/ProductsCounter/ProductsCounterContainer';
 import Products from '../../features/Products/ProductsContainer';
+import './HomePage.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const HomePage = () => (
-  <div>
-  	<ProductsCounter />
-    <Products />
+  <div className="home-page row">
+  	<div className="col-sm-3">
+  		<ProductsCounter />
+  	</div>
+  	<div className="col-sm-9">
+    	<Products />
+    </div>
   </div>
 );
 
