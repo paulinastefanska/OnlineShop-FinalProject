aeecd4dac02f71d74ae3905ba4bf956dfe7cd6fa
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-25 11:43:23 +0200

    Add mongoDB

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const config = require('./config');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -11,6 +12,13 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', productRoutes);
+
+// connects our back end code with the database
+mongoose.connect(config.DB, { useNewUrlParser: true });
+let db = mongoose.connection;
+
+db.once('open', () => console.log('Connected to the database'));
+db.on('error', (err) => console.log('Error ' + err));
 
 app.listen(config.PORT, function(){
   console.log('Server is running on port:', config.PORT);
