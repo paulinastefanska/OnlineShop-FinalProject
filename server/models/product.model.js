7a5c971865a97d8cf68bd9a6a7ca85444a92fb7f
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-26 15:10:45 +0200

    Update data

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Product = new Schema({
-  id: { type: 'String', required: true },
-    img: { type: 'String', required: true },
+    id: { type: 'String', required: true },
+    img: { type: 'Object', required: true },
     name: { type: 'String', required: true },
-    price: { type: 'Number', required: true },
+    price: { type: 'String', required: true },
     desc: { type: 'String', required: true },
 });
 
