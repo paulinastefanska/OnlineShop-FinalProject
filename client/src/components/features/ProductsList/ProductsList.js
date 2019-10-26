72d2e34632ace25347dbf04976e157619a1feb46
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-26 18:22:29 +0200

    Imporve products

--- client/src/components/features/ProductsList/ProductsList.js
+++ client/src/components/features/ProductsList/ProductsList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 
 import ProductsSummary from '../ProductsSummary/ProductsSummary';
+import './ProductsList.scss'
 
 const ProductsList = ({ products }) => (
   <div>
--- client/src/components/features/ProductsSummary/ProductsSummary.js
+++ client/src/components/features/ProductsSummary/ProductsSummary.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import './ProductsSummary.scss';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Card, CardImg, CardText, CardBody, CardTitle, CardLink } from 'reactstrap';
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardLink } from 'reactstrap';
 
 const ProductSummary = ({ img, name, price }) => (
-  <div className={'product-summary'}>
-        <Card>
-            <CardImg src={img}/>
-            <CardBody>
-                <CardTitle>{name}</CardTitle>
-                <CardText>{price}</CardText>
-                <CardLink href={'/product'}></CardLink>
-            </CardBody>
-        </Card>
-    </div>
+  <div className="product-summary">
+    <Card>
+        <CardImg src={img}/>
+        <CardBody>
+            <CardTitle>{name}</CardTitle>
+            <CardSubtitle>{price}</CardSubtitle>
+            <CardLink href={'/product'}></CardLink>
+        </CardBody>
+    </Card>
+  </div>
 );
 
 ProductSummary.propTypes = {
