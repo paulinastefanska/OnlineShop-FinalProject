0d6a26d42f1c02c4f7d6d66837e462caf7934633
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-27 18:02:28 +0100

    Change Link to NavLink

--- client/src/components/features/NavBar/NavBar.js
+++ client/src/components/features/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Logo from '../../common/Logo/Logo';
 import MainMenu from '../../layout/MainMenu/MainMenu';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -24,7 +24,7 @@ class NavBar extends React.Component {
       <nav className="navbar">
         <Logo />
         <MainMenu links={links} />
-        <Link className="cart-icon" to="/cart"><FontAwesomeIcon icon={faShoppingBag} size="2x" /></Link>
+        <NavLink className="cart-icon" to="/cart" activeClassName="active"><FontAwesomeIcon icon={faShoppingBag} size="2x" /></NavLink>
       </nav>
     );
   }
