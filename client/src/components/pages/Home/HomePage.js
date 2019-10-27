6e3d795736e991b84f31f91ad06b08fd9819f0da
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-27 21:15:38 +0100

    Add carousel

--- client/src/components/pages/Home/HomePage.js
+++ client/src/components/pages/Home/HomePage.js
@@ -2,20 +2,22 @@ import React from 'react';
 
 import ProductsCounter from '../../features/ProductsCounter/ProductsCounterContainer';
 import Products from '../../features/Products/ProductsContainer';
+import TopCarousel from '../../../utils/Carousel/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap'; 
 
 const HomePage = () => (
   <Container>
-        <Row>
-            <Col xs={3}>
-                <ProductsCounter />
-            </Col>
-            <Col xs={9}>
-                <Products />
-            </Col>
-        </Row>
-    </Container>
+    <TopCarousel />
+    <Row>
+        <Col xs={3}>
+            <ProductsCounter />
+        </Col>
+        <Col xs={9}>
+            <Products />
+        </Col>
+    </Row>
+  </Container>
 );
 
 export default HomePage;
---
+++ client/src/utils/Carousel/Carousel.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { UncontrolledCarousel } from 'reactstrap';
+import './Carousel.scss';
+
+import Img1 from './sale.jpg';
+import Img2 from './freeshipping.jpg';
+import Img3 from './satisfaction.jpg';
+
+const items = [
+  {
+    src: Img1,
+    altText: 'Slide 1',
+    caption: 'only in TechStore',
+    header: 'best prices',
+    key: '1'
+  },
+  {
+    src: Img2,
+    altText: 'Slide 2',
+    caption: 'only in TechStore',
+    header: 'free shipping',
+    key: '2'
+  },
+  {
+    src: Img3,
+    altText: 'Slide 3',
+    caption: 'only in TechStore',
+    header: 'client satisfaction',
+    key: '3'
+  }
+];
+
+const TopCarousel = () => <UncontrolledCarousel className="carousel" items={items} />;
+
+export default TopCarousel;
