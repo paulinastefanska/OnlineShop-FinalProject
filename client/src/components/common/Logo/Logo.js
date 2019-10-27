25083769c971f0209ade3a3b89b0c2cfb4a446d3
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-27 12:41:57 +0100

    Add logo

--- client/src/components/common/Logo/Logo.js
+++ client/src/components/common/Logo/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from './logo.png';
 
 import './Logo.scss';
 
 const Logo = () => (
-	<Link className="logo" to ="/">
-  	<h1>TechStore</h1>
+  <Link className="logo" to ="/">
+    <img src={logo} alt={'logo'}/>
   </Link>
 );
 
