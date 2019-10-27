aaa11fe608de993173458306632c0f7e07c8f341
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-27 16:47:22 +0100

    Improve products

--- client/src/components/features/ProductsSummary/ProductsSummary.js
+++ client/src/components/features/ProductsSummary/ProductsSummary.js
@@ -2,20 +2,26 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import './ProductsSummary.scss';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardLink } from 'reactstrap';
+import { Col, Card, CardImg, CardBody, CardTitle, CardSubtitle, CardLink } from 'reactstrap';
 
-const ProductSummary = ({ img, name, price }) => (
-  <div className="product-summary">
-    <Card>
-        <CardImg src={img}/>
-        <CardBody>
-            <CardTitle>{name}</CardTitle>
-            <CardSubtitle>{price}</CardSubtitle>
-            <CardLink href={'/product'}></CardLink>
-        </CardBody>
-    </Card>
-  </div>
-);
+export class ProductSummary  extends React.Component  {
+  render() {
+    const { img, name, price } = this.props;
+      
+    return (
+      <Col className="product-summary" xs={6}>
+        <Card>
+            <CardImg src={img.src} alt="item"/>
+            <CardBody>
+                <CardTitle>{name}</CardTitle>
+                <CardSubtitle>{price}</CardSubtitle>
+                <CardLink href={'/product'}></CardLink>
+            </CardBody>
+        </Card>
+      </Col>
+    )  
+  }  
+};
 
 ProductSummary.propTypes = {
   id: PropTypes.string,
