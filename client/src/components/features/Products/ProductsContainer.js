811d8264319a8867ddba929b9d8dd7ab233e7a02
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-11-02 13:02:29 +0100

    Fix small issue

--- client/src/components/features/Products/ProductsContainer.js
+++ client/src/components/features/Products/ProductsContainer.js
@@ -1,12 +1,12 @@
 import { connect } from 'react-redux';
-import { getProductsSort, getRequest, loadProductsByPageRequest, getPages, getpresentPage } from '../../../redux/productRedux';
+import { getProductsSort, getRequest, loadProductsByPageRequest, getPages, getPresentPage } from '../../../redux/productRedux';
 import Products from './Products';
 
 const mapStateToProps = state => ({
   products: getProductsSort(state),
   request: getRequest(state),
   pages: getPages(state),
-  presentPage: getpresentPage(state),
+  presentPage: getPresentPage(state),
 })
 
 const mapDispatchToProps = dispatch => ({
