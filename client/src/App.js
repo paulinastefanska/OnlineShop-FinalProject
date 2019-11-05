e095918938d5afa7ce2fdbcdc90a636725669e46
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-11-05 14:35:30 +0100

    Edit router transition and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { spring, AnimatedSwitch } from 'react-router-transition';
+
+import './App.scss'
 
 // imports routes
 import MainLayout from './components/layout/MainLayout/MainLayout';
@@ -11,23 +14,58 @@ import Cart from './components/pages/Cart/CartPage';
 import SingleProduct from './components/pages/SingleProduct/SingleProductPage';
 import NotFound from './components/pages/NotFound/NotFoundPage';
 
+function mapStyles(styles) {
+  return {
+    opacity: styles.opacity,
+    transform: `scale(${styles.scale})`,
+  };
+}
+
+function bounce(val) {
+  return spring(val, {
+    stiffness: 330,
+    damping: 22,
+  });
+}
+
+const bounceTransition = {
+  atEnter: {
+    opacity: 0,
+    scale: 1.2,
+  },
+  atLeave: {
+    opacity: bounce(0),
+    scale: bounce(0.8),
+  },
+  atActive: {
+    opacity: bounce(1),
+    scale: bounce(1),
+  },
+};
+
 class App extends React.Component {
 
-  render() {
-    return (
-      <MainLayout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/faq" component={Faq} />
-          <Route path="/storepolicy" component={StorePolicy} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/cart" component={Cart} />
-          <Route path="/product/:id" component={SingleProduct} />
-          <Route component={NotFound} />
-        </Switch>
-      </MainLayout>
-    );
-  }
+	render() {
+		return (
+			<MainLayout>
+				<AnimatedSwitch
+				atEnter={bounceTransition.atEnter}
+				atLeave={bounceTransition.atLeave}
+				atActive={bounceTransition.atActive}
+				mapStyles={mapStyles}
+				className="route-wrapper"
+				>
+					<Route exact path="/" component={Home} />
+					<Route path="/faq" component={Faq} />
+					<Route path="/storepolicy" component={StorePolicy} />
+					<Route path="/contact" component={Contact} />
+					<Route path="/cart" component={Cart} />
+					<Route path="/product/:id" component={SingleProduct} />
+					<Route component={NotFound} />
+				</AnimatedSwitch>
+			</MainLayout>
+		);
+	}
 
 };
 
