8b1b09480d78e4c0ff63b861c402a694f596d095
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-11-05 14:12:22 +0100

    Edit router transition

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import { spring, AnimatedSwitch } from 'react-router-transition';
-
-import './App.scss'
+import { Switch, Route } from 'react-router-dom';
 
 // imports routes
 import MainLayout from './components/layout/MainLayout/MainLayout';
@@ -14,47 +11,12 @@ import Cart from './components/pages/Cart/CartPage';
 import SingleProduct from './components/pages/SingleProduct/SingleProductPage';
 import NotFound from './components/pages/NotFound/NotFoundPage';
 
-function mapStyles(styles) {
-  return {
-    opacity: styles.opacity,
-    transform: `scale(${styles.scale})`,
-  };
-}
-
-function bounce(val) {
-  return spring(val, {
-    stiffness: 330,
-    damping: 22,
-  });
-}
-
-const bounceTransition = {
-  atEnter: {
-    opacity: 0,
-    scale: 1.2,
-  },
-  atLeave: {
-    opacity: bounce(0),
-    scale: bounce(0.8),
-  },
-  atActive: {
-    opacity: bounce(1),
-    scale: bounce(1),
-  },
-};
-
 class App extends React.Component {
 
   render() {
     return (
       <MainLayout>
-        <AnimatedSwitch
-          atEnter={bounceTransition.atEnter}
-          atLeave={bounceTransition.atLeave}
-          atActive={bounceTransition.atActive}
-          mapStyles={mapStyles}
-          className="route-wrapper"
-			  >
+        <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/faq" component={Faq} />
           <Route path="/storepolicy" component={StorePolicy} />
@@ -62,7 +24,7 @@ class App extends React.Component {
           <Route path="/cart" component={Cart} />
           <Route path="/product/:id" component={SingleProduct} />
           <Route component={NotFound} />
-        </AnimatedSwitch>
+        </Switch>
       </MainLayout>
     );
   }
