ea1878e38b501c1edc5e68157138d8b3016893d4
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-11-05 10:51:59 +0100

    Add faq page

--- client/src/components/pages/Faq/FaqPage.js
+++ client/src/components/pages/Faq/FaqPage.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
+import PageTitle from '../../common/PageTitle/PageTitle';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'reactstrap';
+import '../StorePolicy/StorePolicyPage.scss'
+
 const FaqPage = () => (
-  <div>
-    <h1>FAQ</h1>
-  </div>
+<Container>
+		<PageTitle>FAQ</PageTitle>
+		<div className="storepolicy-wrapper">
+			<p>Q: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+				eiusmod tempor incididunt ut labore et dolore magna aliqua.
+			</p>
+			<p>A: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+				eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
+				lobortis elementum nibh tellus molestie nunc. Amet venenatis urna cursus
+				eget nunc scelerisque viverra. Odio euismod lacinia at quis risus.
+			</p>
+			<p>Q: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+				eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
+				lobortis elementum nibh tellus molestie nunc.
+			</p>
+			<p>A: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+				eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
+				lobortis elementum nibh tellus molestie nunc. Amet venenatis urna cursus
+				eget nunc scelerisque viverra. Odio euismod lacinia at quis risus.
+			</p>
+		</div>
+	</Container>
 );
 
 export default FaqPage;
--- client/src/components/pages/StorePolicy/StorePolicyPage.js
+++ client/src/components/pages/StorePolicy/StorePolicyPage.js
@@ -8,28 +8,28 @@ import './StorePolicyPage.scss'
 const StorePolicy = () => (
 	<Container>
 		<PageTitle>Store Policy</PageTitle>
-			<div className="storepolicy-wrapper">
-				<p>$1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
-					lobortis elementum nibh tellus molestie nunc. Amet venenatis urna cursus
-					eget nunc scelerisque viverra. Odio euismod lacinia at quis risus.
-				</p>
-				<p>$1.1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
-					lobortis elementum nibh tellus molestie nunc. Amet venenatis urna cursus
-					eget nunc scelerisque viverra. Odio euismod lacinia at quis risus.
-				</p>
-				<p>$1.2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
-					lobortis elementum nibh tellus molestie nunc. Amet venenatis urna cursus
-					eget nunc scelerisque viverra. Odio euismod lacinia at quis risus.
-				</p>
-				<p>$1.3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
-					lobortis elementum nibh tellus molestie nunc. Amet venenatis urna cursus
-					eget nunc scelerisque viverra. Odio euismod lacinia at quis risus.
-				</p>
-			</div>
+		<div className="storepolicy-wrapper">
+			<p>$1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+				eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
+				lobortis elementum nibh tellus molestie nunc. Amet venenatis urna cursus
+				eget nunc scelerisque viverra. Odio euismod lacinia at quis risus.
+			</p>
+			<p>$1.1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+				eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
+				lobortis elementum nibh tellus molestie nunc. Amet venenatis urna cursus
+				eget nunc scelerisque viverra. Odio euismod lacinia at quis risus.
+			</p>
+			<p>$1.2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+				eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
+				lobortis elementum nibh tellus molestie nunc. Amet venenatis urna cursus
+				eget nunc scelerisque viverra. Odio euismod lacinia at quis risus.
+			</p>
+			<p>$1.3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+				eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
+				lobortis elementum nibh tellus molestie nunc. Amet venenatis urna cursus
+				eget nunc scelerisque viverra. Odio euismod lacinia at quis risus.
+			</p>
+		</div>
 	</Container>
 
 );
