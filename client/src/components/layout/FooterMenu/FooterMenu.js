a0cb350cb1526344cf24f9cd6def79c1cf30e030
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-11-12 13:42:02 +0100

    Edit footer

--- client/src/components/layout/FooterMenu/FooterMenu.js
+++ client/src/components/layout/FooterMenu/FooterMenu.js
@@ -8,7 +8,6 @@ const FooterMenu = () => (
         <li><NavLink exact to="/faq" className="navlink">FAQ</NavLink></li>
         <li><NavLink exact to="/storepolicy" className="navlink">Store Policy</NavLink></li>
         <li><NavLink exact to="/contact" className="navlink">Contact</NavLink></li>
-        <li><NavLink exact to="/cart" className="navlink">Cart</NavLink></li>
     </ul>
 
 );
