614e98592d6d0b1f60360cdaedac33fdd87a42ec
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-11-14 20:31:46 +0100

    Edit config to run app locally

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  DB: process.env.DB,
+  DB: process.env.DB ? process.env.DB : 'mongodb+srv://TechStore:TechStore79@cluster0-nq6zb.mongodb.net/test?retryWrites=true&w=majority',
 };
